b310ce5c7452589e2932e27a6c70ba237c8b5ff3
unknown <nsikakatan@gmail.com>
2022-06-14 00:40:03 -0700

    my personal practice on javascript comparison

---
+++ script.js
@@ -0,0 +1,46 @@
+// alert (4 < 5);
+// alert ( 4 == 5);
+// alert (62 < 5);
+// alert (62 != 5);
+// let result = 3 == 4 
+// alert (result)
+
+// if (3 > 4){
+// console.log = ('come collect cigga')}
+// else{
+//     console.log('goaway')
+// };
+
+// alert('glow'<'glee');
+// alert('manson'< 'glory');
+
+// console.log('2' > 1)
+// console.log(03 < 4 )
+// console.log('03'== 3)
+
+// let r = 0
+// console.log( Boolean(r))
+
+// console.log(Boolean(0 === false))
+// console.log ( null == undefined )
+// console.log( null === undefined)
+
+// // for other terms eg. null/undefined
+// console.log( null/undefined)
+
+console.log(null == 0)
+console.log(null <= 0)
+console.log(null >= 0)
+console.log(undefined == null)
+// console.log('2'> '12')
+console.log(undefined == undefined );
+
+
+let a = 0
+console.log(Boolean(a));
+
+let b = '0'
+console.log(Boolean(b));
+
+let c = a === b
+console.log(c);
